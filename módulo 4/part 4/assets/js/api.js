8fdf1110550dd514ccce6ed02762eb3ecb1722cb
Larissa <contatolarisn@gmail.com>
2023-03-10 15:44:55 -0300

    Update api.js

--- módulo 4/part 4/assets/js/api.js
+++ módulo 4/part 4/assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/larisn/bootcamp-javascript-developer/main/m%C3%B3dulo%204/part%204/assets/data/profile.json';
+    const url = 'https://github.com/larisn/bootcamp-javascript-developer/blob/main/m%C3%B3dulo%204/part%204/assets/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
