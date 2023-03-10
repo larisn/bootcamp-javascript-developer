b0bf6facfbe55da0cba514cba7d1070673eb9bad
Larissa <contatolarisn@gmail.com>
2023-03-10 14:14:58 -0300

    modificando link api

--- módulo 4 - currículo online com html, css e javascript - da prototipação ao deploy/part 4/assets/js/api.js
+++ módulo 4 - currículo online com html, css e javascript - da prototipação ao deploy/part 4/assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/RenanJPaula/js-developer-portfolio/main/data/profile.json'
+    const url = 'https://raw.githubusercontent.com/larisn/bootcamp-javascript-developer/main/m%C3%B3dulo%204%20-%20curr%C3%ADculo%20online%20com%20html%2C%20css%20e%20javascript%20-%20da%20prototipa%C3%A7%C3%A3o%20ao%20deploy/part%204/assets/data/profile.json'
     const fetching = await fetch(url)
     return await fetching.json()
 }
