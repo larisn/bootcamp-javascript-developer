85de6258e01457bf6fbe4011f2a8960259d730b2
Larissa <contatolarisn@gmail.com>
2023-03-12 18:59:58 -0300

    update

--- módulo 4 - currículo online com html, css e js/part 1 - criando e estilizando/assets/js/acordeon.js
+++ módulo 4 - currículo online com html, css e js/part 1 - criando e estilizando/assets/js/acordeon.js
@@ -6,7 +6,7 @@ acordeonTriggers.forEach((trigger) => {
         const acordeon = trigger.parentElement
         const isOpen = acordeon.classList.contains('open');
 
-        if (isOpen) {
+        if (isOpen) { 
             acordeon.classList.remove('open');
         } else {
             acordeon.classList.add('open');
